665614ce5e6833d7e5bceed1bc6408ba0de39abb
veerash <veeresh.532000@gmail.com>
2020-06-27 20:46:07 -0400

    removin build

--- ROS_WS/build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/veerash/Rover/ROS_WS/src
-# Build directory: /home/veerash/Rover/ROS_WS/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
-subdirs("ydlidar-master")
--- ROS_WS/build/ydlidar-master/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/veerash/Rover/ROS_WS/src/ydlidar-master
-# Build directory: /home/veerash/Rover/ROS_WS/build/ydlidar-master
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

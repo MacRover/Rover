a1016bc7715e0ca76a19544af6ffbf4a0a582423
veerash <veeresh.532000@gmail.com>
2020-07-29 20:30:08 -0400

    removing devel and build folders

--- ROS_WS/build/simulation/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/veerash/Rover/ROS_WS/src/simulation
-# Build directory: /home/veerash/Rover/ROS_WS/build/simulation
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.

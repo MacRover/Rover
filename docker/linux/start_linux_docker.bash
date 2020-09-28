e3b09f6fd944c4b92e14a67a9890b19898a3e836
emcauliffe <14607273+emcauliffe@users.noreply.github.com>
2020-09-28 00:18:46 -0400

    add non-nvidia linux docker support

---
+++ docker/linux/start_linux_docker.bash
@@ -0,0 +1,36 @@
+XAUTH=/tmp/.docker.xauth
+
+echo "Preparing Xauthority data..."
+xauth_list=$(xauth nlist :0 | tail -n 1 | sed -e 's/^..../ffff/')
+if [ ! -f $XAUTH ]; then
+    if [ ! -z "$xauth_list" ]; then
+        echo $xauth_list | xauth -f $XAUTH nmerge -
+    else
+        touch $XAUTH
+    fi
+    chmod a+r $XAUTH
+fi
+
+echo "Done."
+echo ""
+echo "Verifying file contents:"
+file $XAUTH
+echo "--> It should say \"X11 Xauthority data\"."
+echo ""
+echo "Permissions:"
+ls -FAlh $XAUTH
+echo ""
+echo "Running docker..."
+
+# Include --device="/dev/input/js0" flag to forward a game controller
+
+docker run -it --rm \
+    --name=macrover \
+    --env="DISPLAY=$DISPLAY" \
+    --env="QT_X11_NO_MITSHM=1" \
+    --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+    --env="XAUTHORITY=$XAUTH" \
+    --volume="$XAUTH:$XAUTH" \
+    macrover/rover:linux
+
+echo "Done."

5be0151aa593f4b59bc718422a3d641c619939aa
emcauliffe <14607273+emcauliffe@users.noreply.github.com>
2021-12-04 22:23:56 -0500

    move to structs

--- firmware/dmc_stm32/drive_test.ino
+++ firmware/dmc_stm32/drive_test.ino
@@ -16,26 +16,25 @@
 #define P4_ PA7
 #define P5_ PB0
 
-// global motor variables for use in interrupts
-uint32_t motorP0;
-uint32_t motorP1;
-uint32_t motorP2;
-uint32_t motorP3;
-uint32_t motorP4;
-uint32_t motorP5;
+struct timers
+{
+    HardwareTimer *TimerA;
+    HardwareTimer *TimerB;
+};
 
-// high signals in microseconds
-uint32_t motorP0Pulse = 1500;
-uint32_t motorP1Pulse = 1500;
-uint32_t motorP2Pulse = 1500;
+struct motors
+{
+    uint32_t P0;
+    uint32_t P1;
+    uint32_t P2;
+    uint32_t P3;
+    uint32_t P4;
+    uint32_t P5;
+};
 
-uint32_t motorP3Pulse = 1500;
-uint32_t motorP4Pulse = 1500;
-uint32_t motorP5Pulse = 1500;
-
-// global timer variables
-HardwareTimer *Timer1;
-HardwareTimer *Timer3;
+// global variables for use in interrupts
+motors Motors;
+timers Timers;
 
 // configure stm32 to use Serial2 hardware serial ports
 HardwareSerial hserial(SERIAL_RX, SERIAL_TX);
@@ -46,9 +45,22 @@ public:
 };
 ros::NodeHandle_<NewHardware> nh;
 
-int velToPulse(float vel)
+// subscribe to cmd_vel Twist topic
+void controlMotors(const geometry_msgs::Twist &cmd_vel);
+ros::Subscriber<geometry_msgs::Twist> velocity("cmd_vel", &controlMotors);
+
+void setup()
 {
-    return map(vel, -4, 4, 1000, 2000);
+    configureHardwareTimers();
+    nh.initNode();
+    nh.subscribe(velocity);
+}
+
+void loop()
+{
+    // required for rosserial
+    nh.spinOnce();
+    delay(1);
 }
 
 void controlMotors(const geometry_msgs::Twist &cmd_vel)
@@ -57,18 +69,18 @@ void controlMotors(const geometry_msgs::Twist &cmd_vel)
     float right_speed = cmd_vel.linear.x - cmd_vel.angular.z;
 
     int left_pulse = velToPulse(left_speed);
-    // front motors are reversed
+    // front motors direction are reversed
     int front_left_pulse = velToPulse(left_speed * -1);
 
     int right_pulse = velToPulse(right_speed);
     int front_right_pulse = velToPulse(right_speed * -1);
 
-    Timer1->setCaptureCompare(motorP0, front_left_pulse, MICROSEC_COMPARE_FORMAT);
-    Timer1->setCaptureCompare(motorP1, left_pulse, MICROSEC_COMPARE_FORMAT);
-    Timer1->setCaptureCompare(motorP2, left_pulse, MICROSEC_COMPARE_FORMAT);
-    Timer3->setCaptureCompare(motorP3, front_right_pulse, MICROSEC_COMPARE_FORMAT);
-    Timer3->setCaptureCompare(motorP4, right_pulse, MICROSEC_COMPARE_FORMAT);
-    Timer3->setCaptureCompare(motorP5, right_pulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerA->setCaptureCompare(Motors.P0, front_left_pulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerA->setCaptureCompare(Motors.P1, left_pulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerA->setCaptureCompare(Motors.P2, left_pulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerB->setCaptureCompare(Motors.P3, front_right_pulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerB->setCaptureCompare(Motors.P4, right_pulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerB->setCaptureCompare(Motors.P5, right_pulse, MICROSEC_COMPARE_FORMAT);
 }
 
 void configureHardwareTimers()
@@ -78,55 +90,45 @@ void configureHardwareTimers()
     TIM_TypeDef *Instance1 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(P0_), PinMap_PWM);
     TIM_TypeDef *Instance2 = (TIM_TypeDef *)pinmap_peripheral(digitalPinToPinName(P3_), PinMap_PWM);
 
-    motorP0 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P0_), PinMap_PWM));
-    motorP1 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P1_), PinMap_PWM));
-    motorP2 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P2_), PinMap_PWM));
-    motorP3 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P3_), PinMap_PWM));
-    motorP4 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P4_), PinMap_PWM));
-    motorP5 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P5_), PinMap_PWM));
+    Motors.P0 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P0_), PinMap_PWM));
+    Motors.P1 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P1_), PinMap_PWM));
+    Motors.P2 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P2_), PinMap_PWM));
+    Motors.P3 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P3_), PinMap_PWM));
+    Motors.P4 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P4_), PinMap_PWM));
+    Motors.P5 = STM_PIN_CHANNEL(pinmap_function(digitalPinToPinName(P5_), PinMap_PWM));
 
     // Instantiate HardwareTimer object. Thanks to 'new' instantiation, HardwareTimer is not destructed when setup() function is finished.
-    Timer1 = new HardwareTimer(Instance1);
-    Timer3 = new HardwareTimer(Instance2);
+    Timers.TimerA = new HardwareTimer(Instance1);
+    Timers.TimerB = new HardwareTimer(Instance2);
 
     // Configure and start PWM
-    Timer1->setMode(motorP0, TIMER_OUTPUT_COMPARE_PWM1, P0_);
-    Timer1->setMode(motorP1, TIMER_OUTPUT_COMPARE_PWM1, P1_);
-    Timer1->setMode(motorP2, TIMER_OUTPUT_COMPARE_PWM1, P2_);
-    Timer3->setMode(motorP3, TIMER_OUTPUT_COMPARE_PWM1, P3_);
-    Timer3->setMode(motorP4, TIMER_OUTPUT_COMPARE_PWM1, P4_);
-    Timer3->setMode(motorP5, TIMER_OUTPUT_COMPARE_PWM1, P5_);
+    Timers.TimerA->setMode(Motors.P0, TIMER_OUTPUT_COMPARE_PWM1, P0_);
+    Timers.TimerA->setMode(Motors.P1, TIMER_OUTPUT_COMPARE_PWM1, P1_);
+    Timers.TimerA->setMode(Motors.P2, TIMER_OUTPUT_COMPARE_PWM1, P2_);
+    Timers.TimerB->setMode(Motors.P3, TIMER_OUTPUT_COMPARE_PWM1, P3_);
+    Timers.TimerB->setMode(Motors.P4, TIMER_OUTPUT_COMPARE_PWM1, P4_);
+    Timers.TimerB->setMode(Motors.P5, TIMER_OUTPUT_COMPARE_PWM1, P5_);
 
     // complete signal period set to 3us
-    Timer1->setOverflow(3 * 1000, MICROSEC_FORMAT);
-    Timer3->setOverflow(3 * 1000, MICROSEC_FORMAT);
+    Timers.TimerA->setOverflow(3 * 1000, MICROSEC_FORMAT);
+    Timers.TimerB->setOverflow(3 * 1000, MICROSEC_FORMAT);
 
     // set initial pulse of 1500us (stopped)
-    Timer1->setCaptureCompare(motorP0, motorP0Pulse, MICROSEC_COMPARE_FORMAT);
-    Timer1->setCaptureCompare(motorP1, motorP1Pulse, MICROSEC_COMPARE_FORMAT);
-    Timer1->setCaptureCompare(motorP2, motorP2Pulse, MICROSEC_COMPARE_FORMAT);
-    Timer3->setCaptureCompare(motorP3, motorP3Pulse, MICROSEC_COMPARE_FORMAT);
-    Timer3->setCaptureCompare(motorP4, motorP4Pulse, MICROSEC_COMPARE_FORMAT);
-    Timer3->setCaptureCompare(motorP5, motorP5Pulse, MICROSEC_COMPARE_FORMAT);
+    int initialPulse = 1500;
+
+    Timers.TimerA->setCaptureCompare(Motors.P0, initialPulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerA->setCaptureCompare(Motors.P1, initialPulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerA->setCaptureCompare(Motors.P2, initialPulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerB->setCaptureCompare(Motors.P3, initialPulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerB->setCaptureCompare(Motors.P4, initialPulse, MICROSEC_COMPARE_FORMAT);
+    Timers.TimerB->setCaptureCompare(Motors.P5, initialPulse, MICROSEC_COMPARE_FORMAT);
 
     // enable timers
-    Timer1->resume();
-    Timer3->resume();
+    Timers.TimerA->resume();
+    Timers.TimerB->resume();
 }
 
-// subscribe to cmd_vel Twist topic
-ros::Subscriber<geometry_msgs::Twist> sub("cmd_vel", &controlMotors);
-
-void setup()
+int velToPulse(float vel)
 {
-    configureHardwareTimers();
-    nh.initNode();
-    nh.subscribe(sub);
-}
-
-void loop()
-{
-    // required for rosserial
-    nh.spinOnce();
-    delay(1);
+    return map(vel, -4, 4, 1000, 2000);
 }

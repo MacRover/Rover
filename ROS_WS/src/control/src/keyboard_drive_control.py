26a360e2b58f6d0ce343dace2f6afd986bb57c04
emcauliffe <14607273+emcauliffe@users.noreply.github.com>
2022-02-10 22:22:18 -0500

    maximize drive speed control granularity also remove 4 wheel system code

--- ROS_WS/src/control/src/keyboard_drive_control.py
+++ ROS_WS/src/control/src/keyboard_drive_control.py
@@ -3,48 +3,53 @@ import rospy
 import sys, termios, tty, os, time
 from geometry_msgs.msg import Twist
 
-def getch(): #this function is used to get key input from user in the terminal
+
+def getch():  # this function is used to get key input from user in the terminal
     fd = sys.stdin.fileno()
     old_settings = termios.tcgetattr(fd)
     try:
         tty.setraw(sys.stdin.fileno())
         ch = sys.stdin.read(1)
- 
+
     finally:
         termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
     return ch
- 
-button_delay = 0.2
 
+
+button_delay = 0.2
 
 
 def move():
     rospy.init_node("car_controller", anonymous=False)
-    publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    publisher = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
     vel_msg = Twist()
 
-    print("Press w, a, s, d accordingly to move; space for stopping and Esckey for exiting. Be careful not to hold these keys, as these keys control the acceleration of the bot, and u dont want the bot flying out!!")
+    print(
+        "Press w, a, s, d accordingly to move; space for stopping and Esckey for exiting. Be careful not to hold these keys, as these keys control the acceleration of the bot, and u dont want the bot flying out!!"
+    )
     while True:
         char = getch()
-        if ord(char)==27:
+        if ord(char) == 27:
             print("Exiting")
             exit(0)
-        elif char=="w":
+        elif char == "w":
             print("Accelerated")
-            vel_msg.linear.x +=0.1
-        elif char=="s":
+            vel_msg.linear.x += 0.008
+        elif char == "s":
             print("Accelerated the opposite direction")
-            vel_msg.linear.x -=0.1
-        elif char=="d":
+            vel_msg.linear.x -= 0.008
+        elif char == "d":
             print("Turning right")
-            vel_msg.angular.z +=0.1
-        elif char=="a":
+            vel_msg.angular.z += 0.008
+        elif char == "a":
             print("Turning left")
-            vel_msg.angular.z -=0.1
-        elif ord(char)==32:
+            vel_msg.angular.z -= 0.008
+        elif ord(char) == 32:
             print("Stopping")
             vel_msg.linear.x = 0
             vel_msg.angular.z = 0
+
+        print("x vel: {}\tz vel: {}".format(vel_msg.linear.x, vel_msg.angular.z))
 
         vel_msg.linear.y = 0
         vel_msg.linear.z = 0
@@ -53,10 +58,9 @@ def move():
 
         publisher.publish(vel_msg)
 
-if __name__== '__main__':
+
+if __name__ == "__main__":
     try:
         move()
     except rospy.ROSInterruptException:
         pass
-    
-

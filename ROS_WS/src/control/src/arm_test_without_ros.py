29b6f221a20da5759ed36690e6e86ce9bdb2cb1e
rutheniumvi <veeresh.532000@gmail.com>
2022-08-13 11:17:49 -0400

    uploading syboar code

--- ROS_WS/src/control/src/arm_test_without_ros.py
+++ ROS_WS/src/control/src/arm_test_without_ros.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # Importing Libraries
 import serial
-import getch
 import serial.tools.list_ports
-import sys, termios, tty, os, time
+import time
+import struct
 
 def getBlackPillPort():
-    return "/dev/ttyTHS2"
+    return "COM4"
     portList = list(serial.tools.list_ports.comports())
     for port in portList:
         if "BLACKPILL" in port[1]:
@@ -14,23 +14,17 @@ def getBlackPillPort():
 
 if __name__ == "__main__":
 
-    print("These are the keys used to control the arm:\n\
-            W, S for the upper arm,\n\
-            A, D for the base hinge,\n\
-            T, G for the forearm,\n\
-            F, H for the wrist,\n\
-            I, K for the claw base,\n\
-            J, L for the claws,\n\
-            Esc to end program\n\
-    ")
-
     blackpill = arduino = serial.Serial(port=getBlackPillPort(), baudrate=115200)
 
     while True:
-        char = getch.getch()
-        if ord(char)==27:
+        command = input("Enter a command number: ")
+        if command == "exit":
             print("Exiting")
             exit(0)
-        
-        blackpill.write(char.encode())
+
+        command_int = int(command)
+        data = struct.pack("<B", command_int)
+        arduino.write(data)
         time.sleep(0.05)
+        data = arduino.read()
+        print(data)

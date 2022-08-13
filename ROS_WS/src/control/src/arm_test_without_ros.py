1714ac176c836de495cf1c053b82f62e8a990af5
rutheniumvi <veeresh.532000@gmail.com>
2022-08-13 11:20:06 -0400

    upladoing syboard code

--- ROS_WS/src/control/src/arm_test_without_ros.py
+++ ROS_WS/src/control/src/arm_test_without_ros.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 # Importing Libraries
 import serial
+import getch
 import serial.tools.list_ports
-import time
-import struct
+import sys, termios, tty, os, time
 
 def getBlackPillPort():
-    return "COM4"
+    return "/dev/ttyTHS2"
     portList = list(serial.tools.list_ports.comports())
     for port in portList:
         if "BLACKPILL" in port[1]:
@@ -14,17 +14,23 @@ def getBlackPillPort():
 
 if __name__ == "__main__":
 
+    print("These are the keys used to control the arm:\n\
+            W, S for the upper arm,\n\
+            A, D for the base hinge,\n\
+            T, G for the forearm,\n\
+            F, H for the wrist,\n\
+            I, K for the claw base,\n\
+            J, L for the claws,\n\
+            Esc to end program\n\
+    ")
+
     blackpill = arduino = serial.Serial(port=getBlackPillPort(), baudrate=115200)
 
     while True:
-        command = input("Enter a command number: ")
-        if command == "exit":
+        char = getch.getch()
+        if ord(char)==27:
             print("Exiting")
             exit(0)
-
-        command_int = int(command)
-        data = struct.pack("<B", command_int)
-        arduino.write(data)
+        
+        blackpill.write(char.encode())
         time.sleep(0.05)
-        data = arduino.read()
-        print(data)
---
+++ ROS_WS/src/control/src/syboard_test_without_ros.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# Importing Libraries
+import serial
+import serial.tools.list_ports
+import time
+import struct
+
+def getBlackPillPort():
+    return "COM4"
+    portList = list(serial.tools.list_ports.comports())
+    for port in portList:
+        if "BLACKPILL" in port[1]:
+            return port[0]
+
+if __name__ == "__main__":
+
+    blackpill = arduino = serial.Serial(port=getBlackPillPort(), baudrate=115200)
+
+    while True:
+        command = input("Enter a command number: ")
+        if command == "exit":
+            print("Exiting")
+            exit(0)
+
+        command_int = int(command)
+        data = struct.pack("<B", command_int)
+        arduino.write(data)
+        time.sleep(0.05)
+        data = arduino.read()
+        print(data)

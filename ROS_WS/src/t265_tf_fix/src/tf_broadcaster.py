d21695336c82d3b33da998a4a42678c6263d9e62
Haashim Rehan <haashimr@hotmail.com>
2023-05-07 22:36:40 -0400

    added t265_tf_fix package

---
+++ ROS_WS/src/t265_tf_fix/src/tf_broadcaster.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import rospy
+import tf
+import tf2_ros
+from geometry_msgs.msg import TransformStamped
+
+if __name__ == '__main__':
+    rospy.init_node('tf_broadcaster')
+    print("=== Starting tf_braodcaster ===")
+
+    broadcaster = tf2_ros.StaticTransformBroadcaster()
+    static_tf_0 = TransformStamped()
+
+
+    while not rospy.is_shutdown():
+        try:
+            static_tf_0.header.stamp = rospy.Time.now()
+            static_tf_0.header.frame_id = "t265_camera_pose_frame"
+            static_tf_0.child_frame_id = "fake_base_footprint"
+        
+            static_tf_0.transform.translation.x = float('-0.32')
+            static_tf_0.transform.translation.y = float('-0.00')
+            static_tf_0.transform.translation.z = float('-0.16')
+        
+            quat = tf.transformations.quaternion_from_euler(float('0.00'),float('0.00'),float('0.00'))
+            static_tf_0.transform.rotation.x = quat[0]
+            static_tf_0.transform.rotation.y = quat[1]
+            static_tf_0.transform.rotation.z = quat[2]
+            static_tf_0.transform.rotation.w = quat[3]
+            broadcaster.sendTransform([static_tf_0])
+        
+        except rospy.ROSInterruptException:
+            pass
+        
+        rospy.spin()
---
+++ ROS_WS/src/t265_tf_fix/src/tf_to_odom.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+import rospy
+import tf
+from nav_msgs.msg import Odometry
+#import time
+
+def callback(rs_odom):
+    global lx, ly, lz, ax, ay, az
+    lx = rs_odom.twist.twist.linear.x
+    ly = rs_odom.twist.twist.linear.y
+    lz = rs_odom.twist.twist.linear.z
+    
+    ax = rs_odom.twist.twist.angular.x
+    ay = rs_odom.twist.twist.angular.y
+    az = rs_odom.twist.twist.angular.z
+
+
+if __name__ == '__main__':
+    rospy.init_node('tf_to_odom')
+    print("=== Starting tf_to_odom ===")
+        
+    listener = tf.TransformListener()
+    odom_sub = rospy.Subscriber("/t265_camera/odom/sample", Odometry, callback)
+    odom_pub = rospy.Publisher("/rs_t265_odom", Odometry , queue_size=10)
+        
+    odom_msg = Odometry()
+    
+    odom_msg.header.frame_id = "ekf_odom"
+    odom_msg.child_frame_id = "rs_base_link"
+    
+    odom_msg.pose.covariance = [0.01, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                0.0, 0.01, 0.0, 0.0, 0.0, 0.0,
+                                0.0, 0.0, 0.01, 0.0, 0.0, 0.0,
+                                0.0, 0.0, 0.0, 0.0001, 0.0, 0.0,
+                                0.0, 0.0, 0.0, 0.0, 0.0001, 0.0,
+                                0.0, 0.0, 0.0, 0.0, 0.0, 0.0001]
+                                        
+    odom_msg.twist.covariance = [0.01, 0.0, 0.0, 0.0, 0.0, 0.0,
+                                0.0, 0.01, 0.0, 0.0, 0.0, 0.0,
+                                0.0, 0.0, 0.01, 0.0, 0.0, 0.0,
+                                0.0, 0.0, 0.0, 0.0001, 0.0, 0.0,
+                                0.0, 0.0, 0.0, 0.0, 0.0001, 0.0,
+                                0.0, 0.0, 0.0, 0.0, 0.0, 0.0001]
+                                
+    while not rospy.is_shutdown():   
+        try:    
+            rospy.loginfo("odom info recieved %s"%(str([[lx,ly,lz],[ax,ay,az]])))
+            rospy.loginfo("beginning odometry transformation")
+        except NameError:
+            continue
+        else:
+            break
+    
+    rate = rospy.Rate(50)
+    try:
+        while not rospy.is_shutdown():
+            try:
+                (trans,rot) = listener.lookupTransform('/t265_camera_odom_frame', '/fake_base_footprint', rospy.Time(0))
+            except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+                continue
+                
+            odom_msg.pose.pose.position.x = trans[0]
+            odom_msg.pose.pose.position.y = trans[1]
+            odom_msg.pose.pose.position.z = trans[2]
+            odom_msg.pose.pose.orientation.x = rot[0]
+            odom_msg.pose.pose.orientation.y = rot[1]
+            odom_msg.pose.pose.orientation.z = rot[2]
+            odom_msg.pose.pose.orientation.w = rot[3]
+                        
+            odom_msg.twist.twist.linear.x = lx
+            #odom_msg.twist.twist.linear.y = ly
+            #odom_msg.twist.twist.linear.z = lz
+            odom_msg.twist.twist.angular.x = ax
+            odom_msg.twist.twist.angular.y = ay
+            odom_msg.twist.twist.angular.z = az
+            
+            now  = rospy.get_rostime()
+            odom_msg.header.stamp.secs = now.secs
+            odom_msg.header.stamp.nsecs = now.nsecs
+            
+            odom_pub.publish(odom_msg)            
+                
+            rate.sleep()
+            
+    except rospy.ROSInterruptException:
+        pass

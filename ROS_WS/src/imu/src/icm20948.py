f2b57eb90eb2edb85b9568855a98968224648f1c
Haashim Rehan <haashimr@hotmail.com>
2023-05-13 22:39:03 -0400

    fixed imu not publishing bug

--- ROS_WS/src/imu/src/icm20948.py
+++ ROS_WS/src/imu/src/icm20948.py
@@ -33,37 +33,36 @@ def icm20948_node():
         rospy.loginfo(message)
         return
 
-    IMU.begin()    
+    IMU.begin()
 
     while not rospy.is_shutdown():
         if IMU.dataReady():
             IMU.getAgmt()
             raw_msg = Imu()
             raw_msg.header.stamp = rospy.Time.now()
-	            
+
             raw_msg.orientation.w = 0
             raw_msg.orientation.x = 0
             raw_msg.orientation.y = 0
             raw_msg.orientation.z = 0
-                
+
             raw_msg.linear_acceleration.x = RawToMeterPerSecond(IMU.axRaw)
             raw_msg.linear_acceleration.y = RawToMeterPerSecond(IMU.ayRaw)
             raw_msg.linear_acceleration.z = RawToMeterPerSecond(IMU.azRaw)
-                
+
             raw_msg.angular_velocity.x = RawToRadsPerSecond(IMU.gxRaw)
             raw_msg.angular_velocity.y = RawToRadsPerSecond(IMU.gyRaw)
             raw_msg.angular_velocity.z = RawToRadsPerSecond(IMU.gzRaw)
-                
+
             raw_msg.orientation_covariance[0] = -1
             raw_msg.linear_acceleration_covariance[0] = -1
             raw_msg.angular_velocity_covariance[0] = -1
-                
+
             raw_pub.publish(raw_msg)
-            
 
             mag_msg = MagneticField()
             mag_msg.header.stamp = rospy.Time.now()
-            
+
             # switch X/Y and invert Z to convert from NED to ENU
             mag_msg.magnetic_field.x = RawToTesla(IMU.myRaw)
             mag_msg.magnetic_field.y = RawToTesla(IMU.mxRaw)
@@ -71,9 +70,8 @@ def icm20948_node():
             mag_msg.magnetic_field_covariance[0] = -1
             mag_pub.publish(mag_msg)
 
-            rospy.spin()
-            rate.sleep()   
-    
+        rate.sleep()
+
     rospy.loginfo(rospy.get_caller_id() + "  icm20948 node finished")
 
 if __name__ == '__main__':

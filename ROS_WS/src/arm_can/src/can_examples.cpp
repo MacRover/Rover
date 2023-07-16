f71aa6792e6209552a124430e861196ad41f1309
emcauliffe <14607273+emcauliffe@users.noreply.github.com>
2023-07-16 00:05:46 -0400

    basic control of arm joints over CAN moved cpp to examples

--- ROS_WS/src/arm_can/src/can_examples.cpp
+++ ROS_WS/src/arm_can/src/can_examples.cpp
@@ -2,6 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
 
 #include <net/if.h>
 #include <sys/ioctl.h>
@@ -95,7 +97,25 @@ int build_can_frame(const canid_t can_id, const u_int8_t dlc, const u_int8_t *da
     return 0;
 }
 
-int main(int argc, char **argv)
+/**
+ * @brief Construct a CAN filter
+ * @param can_id
+ * @param can_mask
+ * @param target_filter
+ * @return
+ */
+int build_can_filter(const canid_t can_id, const canid_t can_mask, can_filter &target_filter)
+{
+    target_filter.can_id = can_id;
+    target_filter.can_mask = can_mask;
+}
+
+void set_can_filters(const can_filter filters[], int &target_socket)
+{
+    setsockopt(target_socket, SOL_CAN_RAW, CAN_RAW_FILTER, &filters, sizeof(filters));
+}
+
+int send_example()
 {
     int s;
     struct can_frame frame;
@@ -109,6 +129,70 @@ int main(int argc, char **argv)
     send_can(s, frame);
 
     close_can(s);
+    return 0;
+}
+
+void callback(int signum)
+{
+    printf("hello world\n");
+}
+
+int filter_example()
+{
+    int s;
+    int nbytes;
+    struct sockaddr_can addr;
+    struct ifreq ifr;
+    struct can_frame frame;
+    const char device[] = "vcan0";
+
+    signal(SIGIO, callback);
+
+    printf("CAN Sockets Receive Filter Demo\r\n");
+
+    init_can(s, device);
+
+    struct can_filter rfilter[1];
+    build_can_filter(0x550, 0xFF0, rfilter[0]);
+    set_can_filters(rfilter, s);
+
+    // fcntl(s, SIGIO);
+    if (fcntl(s, F_SETOWN) < 0)
+    {
+        perror("fcntl F_SETOWN");
+        exit(1);
+    }
+
+    while (true)
+        ;
+
+    // nbytes = read(s, &frame, sizeof(struct can_frame));
+
+    // if (nbytes < 0)
+    // {
+    //     perror("Read");
+    //     return 1;
+    // }
+
+    printf("0x%03X [%d] ", frame.can_id, frame.can_dlc);
+
+    for (int i = 0; i < frame.can_dlc; i++)
+        printf("%02X ", frame.data[i]);
+
+    printf("\r\n");
+
+    close_can(s);
+
+    return 0;
+}
+
+int main(int argc, char **argv)
+{
+    // send_example();
+
+    // receive_example();
+
+    filter_example();
 
     return 0;
 }

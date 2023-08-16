6599803274fc3f2b7b512893f7d31827e11738af
Haashim Rehan <haashimr@hotmail.com>
2023-08-16 17:50:31 -0600

    use xbox controller with usb cable

--- ROS_WS/src/control/src/controller_drive_control.py
+++ ROS_WS/src/control/src/controller_drive_control.py
@@ -19,12 +19,12 @@ def callback(data):
     #)
     vel_msg.linear.x = map_(data.axes[1], -1.0, 1.0, -TOP_SPEED, TOP_SPEED)
 
-    right_stick_direction = data.axes[2]
+    right_stick_direction = data.axes[3]
     print(vel_msg.linear.x)
     if right_stick_direction < dead_zone and right_stick_direction > -1.0 * dead_zone:
         vel_msg.angular.z = 0
     else:
-        vel_msg.angular.z = -map_(right_stick_direction, -1.0, 1.0, TOP_SPEED, -TOP_SPEED)
+        vel_msg.angular.z = -map_(right_stick_direction, -1.0, 1.0, 0.7, -0.7)
     global pub
 
     # press A to stop
